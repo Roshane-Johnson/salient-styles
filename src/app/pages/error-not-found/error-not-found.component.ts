31654e9fc31a792bbdf80507ff80edc252c35bd5
Roshane <39389600+Roshane-Johnson@users.noreply.github.com>
2022-04-09 22:16:32 -0500

    created error not found component

---
+++ src/app/pages/error-not-found/error-not-found.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './error-not-found.component.html',
+  styleUrls: ['./error-not-found.component.scss'],
+})
+export class ErrorNotFoundComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
