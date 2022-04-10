f4abc2d22b1ba54475e7506da0edb6e4ecba4b6e
Roshane <39389600+Roshane-Johnson@users.noreply.github.com>
2022-04-09 22:15:49 -0500

    created gradient card component

--- src/app/components/gradient-card/gradient-card.component.ts
+++ src/app/components/gradient-card/gradient-card.component.ts
@@ -1,4 +1,13 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChildren,
+} from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from 'src/app/services/auth.service';
+import { Gradient } from 'src/app/types/Gradient';
 
 @Component({
   selector: 'app-gradient-card',
@@ -6,21 +15,67 @@ import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./gradient-card.component.scss'],
 })
 export class GradientCardComponent implements OnInit {
-  @Input() gradient: string = '';
+  constructor(
+    private snackbar: MatSnackBar,
+    private authService: AuthService
+  ) {}
 
-  @ViewChild('gradient_card') gradient_card!: ElementRef;
+  @Input() gradient: Gradient = {
+    id: 0,
+    name: 'Hyper',
+    colors: 'rgb(233, 213, 255), rgb(192, 132, 252), rgb(107, 33, 168)',
+    is_favorite: false,
+  };
 
-  constructor() {}
+  @ViewChildren('gradientElement') gradientElement: ElementRef[] = [];
+
+  gradientColors: string[] = [];
+  isLoggedIn = this.authService.loggedIn();
 
   ngOnInit(): void {}
 
-  copyColor(colorIndex: number, ...args: any[]): void {
-    navigator.clipboard.writeText(this.gradient[colorIndex]);
+  /**
+   * A callback method that is invoked after the page view is initialized for the first time.
+   * It is invoked only once when the directive is instantiated.
+   */
+  ngAfterViewInit(): void {
+    this.gradientElement.forEach((element: ElementRef) => {
+      element.nativeElement.style.background = `linear-gradient(to right, ${this.gradient.colors}) no-repeat`;
+
+      let gradientBackground = element.nativeElement.style.background;
+
+      // The set time out here forces Angular to put this function in the next change detection cycle instead of the current
+      // Bug Bypass: https://angular.io/errors/NG0100
+      setTimeout(() => {
+        this.gradientColors = Array.from(
+          gradientBackground.matchAll(/rgb\(\d{1,3}, \d{1,3}, \d{1,3}\)/g)
+        ).map((match: any) => {
+          return match.toString();
+        });
+      }, 0);
+    });
   }
 
-  ngAfterViewInit(): void {
-    // this.gradient = this.gradient ?? 'dodgerblue, blue';
+  /**
+   * A helper method that copies the css gradient for the selected component by fetching the elements css background property
+   * @param clickedElement The element that has the gradient style on the background property
+   */
+  copyGradient(clickedElement: HTMLElement): void {
+    const gradient = `background: ${clickedElement.style.background};`;
+    navigator.clipboard.writeText(gradient);
+    this.snackbar.open('Gradient copied to clipboard', 'OK', {
+      duration: 5000,
+    });
+  }
 
-    this.gradient_card.nativeElement.style.background = `linear-gradient(to right, ${this.gradient})`;
+  /**
+   * A helper method that write the specified color in an array to the users clipboard
+   * @param color Writes the color passed to the function to the users clipboard
+   */
+  copyColor(color: string): void {
+    navigator.clipboard.writeText(color);
+    this.snackbar.open('Color copied to clipboard', 'OK', {
+      duration: 5000,
+    });
   }
 }
