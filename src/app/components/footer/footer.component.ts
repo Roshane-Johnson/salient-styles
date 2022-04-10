836c543bc7c2151e4def5a3fcc884818ae9a4599
Roshane <39389600+Roshane-Johnson@users.noreply.github.com>
2022-04-09 22:15:29 -0500

    created footer component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  env = environment;
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
